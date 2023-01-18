e00d99cc8b9f2614c6ad815ff01f2193b1e6b366
wamsi98-neighbor <wamsi98@neighbor21.co.kr>
2023-01-18 12:13:12 +0900

    0118 1213 First Commit

---
+++ src/main/java/com/example/demo/TestProject3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProject3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/testController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController { 
+
+	@RequestMapping("/test")
+	public String test1(Model model) {
+		
+		model.addAttribute("testMsg", "Thymeleaf Test . ");
+		
+		return "test";
+	}  
+	
+} 
+
